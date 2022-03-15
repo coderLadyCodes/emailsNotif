443d3e7ae102c5b63ad104548cd17633b1d07c4d
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-15 06:36:03 +0100

    Rest controller

--- src/main/java/com/samia/notification/NotificationControlller.java
+++ src/main/java/com/samia/notification/NotificationControlller.java
@@ -1,12 +1,11 @@
 package com.samia.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class NotificationControlller {
     @Autowired
     private Schedule schedule;

c023ac3356b7a651bcbc9b96b3cf718387a3b5cf
jeSuisCodeuse <creativegirl@outlook.fr>
2022-03-15 06:51:18 +0100

    weird ndpoint

--- src/main/java/com/samia/notification/NotificationControlller.java
+++ src/main/java/com/samia/notification/NotificationControlller.java
@@ -13,7 +13,7 @@ public class NotificationControlller {
     @Autowired
     private EmailSending emailSending;
 
-    @GetMapping("/notifs")
+    @GetMapping("/")
      public String helloWorld() {
          return "hello";
      }

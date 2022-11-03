2e7bf904bd6d1a0d80b3fe6a10e0a129de1a9ac6
jeSuisCodeuse <creativegirl@outlook.fr>
2022-11-03 12:34:54 +0100

    cron once a month

--- src/main/java/com/samia/notification/NotificationControlller.java
+++ src/main/java/com/samia/notification/NotificationControlller.java
@@ -1,11 +1,13 @@
 package com.samia.notification;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+//@RestController
+@Controller
 public class NotificationControlller {
     @Autowired
     private Schedule schedule;
